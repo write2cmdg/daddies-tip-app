41091935d8b599a8b868f278743be3c7e6a6ef63
Cristian Di Grandi <write2cmdg@gmail.com>
2024-03-27 15:32:07 -0400

    formatted currency

--- src/components/TransactionItem.jsx
+++ src/components/TransactionItem.jsx
@@ -5,13 +5,15 @@ import { ArrowsPointingOutIcon } from '@heroicons/react/24/outline'
 
 const TransactionItem = ({ transaction }) => {
     const rawPayment = transaction.payment
+    const checkFormatted = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(transaction.check);
+  const tipsFormatted = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(transaction.tips);
 
 
 
   return (
     <>
-      <td>{transaction.check}</td>
-      <td>{transaction.tips}</td>
+      <td>{checkFormatted}</td>
+      <td>{tipsFormatted}</td>
       <td>{rawPayment.includes('Card') ? rawPayment.replace('Card', ' Card') : rawPayment}</td>
       <td> <Link to='/' className='btn'><ArrowsPointingOutIcon height={20} /></Link> </td>
     </>
